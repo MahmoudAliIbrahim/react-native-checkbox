70cc0c25e291726a62efb2e7e7878e7e672ab66a
Mahmoud Ali Ibrahim <ma7moud.saraya@gmail.com>
2017-04-08 00:53:15 +0200

    code refactoring - make checkbox change only when clicking over image

--- checkbox.js
+++ checkbox.js
@@ -1,7 +1,5 @@
-'use strict';
-
 import React, { Component } from 'react';
-var PropTypes = React.PropTypes;
+
 import {
     StyleSheet,
     Image,
@@ -9,130 +7,154 @@ import {
     View,
     TouchableWithoutFeedback,
 } from 'react-native';
+
+const PropTypes = React.PropTypes;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  checkbox: {
+    width: 26,
+    height: 26,
+  },
+  labelContainer: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  label: {
+    fontSize: 15,
+    color: 'grey',
+  },
+});
+
 const CB_ENABLED_IMAGE = require('./cb_enabled.png');
 const CB_DISABLED_IMAGE = require('./cb_disabled.png');
 
 class CheckBox extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            internalChecked: false
-        };
+    this.state = {
+      internalChecked: false,
+    };
 
-        this.onChange = this.onChange.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange() {
+    if (this.props.onChange && typeof this.props.checked === 'boolean') {
+      this.props.onChange(this.props.checked);
+    } else {
+      const internalChecked = this.state.internalChecked;
+
+      if (this.props.onChange) {
+        this.props.onChange(internalChecked);
+      }
+      this.setState({
+        internalChecked: !internalChecked,
+      });
     }
+  }
 
-    onChange() {
-        if (this.props.onChange &&  typeof this.props.checked === 'boolean') {
-            this.props.onChange(this.props.checked);
-        } else {
-            let internalChecked = this.state.internalChecked;
-
-            if(this.props.onChange){
-              this.props.onChange(internalChecked);
-            }
-            this.setState({
-                internalChecked: !internalChecked
-            });
-        }
-    }
-
-    render() {
-        let container = (
+  render() {
+    let source;
+    let container = (
             <View style={this.props.containerStyle || styles.container}>
                 <Image
-                style={this.props.checkboxStyle || styles.checkbox}
-                source={source}/>
+                    source={source}
+                    style={this.props.checkboxStyle || styles.checkbox}
+                />
                 <View style={styles.labelContainer}>
                     <Text style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
                 </View>
             </View>
         );
 
-        let source;
-
-        if(typeof this.props.checked === 'boolean') {
-          source = this.props.checked ? this.props.checkedImage : this.props.uncheckedImage;
-        } else {
-          source = this.state.internalChecked ? this.props.checkedImage : this.props.uncheckedImage;
-        }
-
-
-        if (this.props.labelBefore) {
-            container = (
-                <View style={this.props.containerStyle || [styles.container, styles.flexContainer]}>
-                    <View style={styles.labelContainer}>
-                        <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-                    </View>
-                    <Image
-                    style={[styles.checkbox, this.props.checkboxStyle]}
-                    source={source}/>
-                </View>
-            );
-        } else {
-            container = (
-                <View style={[styles.container, this.props.containerStyle]}>
-                    <Image
-                    style={[styles.checkbox, this.props.checkboxStyle]}
-                    source={source}/>
-                    <View style={styles.labelContainer}>
-                        <Text numberOfLines={this.props.labelLines} style={[styles.label, this.props.labelStyle]}>{this.props.label}</Text>
-                    </View>
-                </View>
-            );
-        }
-
-        return (
-            <TouchableWithoutFeedback onPress={this.onChange} underlayColor={this.props.underlayColor} style={styles.flexContainer}>
-                {container}
-            </TouchableWithoutFeedback>
-        );
+    if (typeof this.props.checked === 'boolean') {
+      source = this.props.checked ? this.props.checkedImage : this.props.uncheckedImage;
+    } else {
+      source = this.state.internalChecked ? this.props.checkedImage : this.props.uncheckedImage;
     }
+
+
+    if (this.props.labelBefore) {
+      container = (
+              <View
+                  style={this.props.containerStyle || [styles.container, styles.flexContainer]}
+              >
+                  <View style={styles.labelContainer}>
+                      <Text
+                          numberOfLines={this.props.labelLines}
+                          style={[styles.label, this.props.labelStyle]}
+                      >
+                        {this.props.label}
+                      </Text>
+                  </View>
+                  <TouchableWithoutFeedback onPress={this.onChange}>
+                    <Image
+                        source={source}
+                        style={[styles.checkbox, this.props.checkboxStyle]}
+                    />
+                  </TouchableWithoutFeedback>
+              </View>
+          );
+    } else {
+      container = (
+              <View style={[styles.container, this.props.containerStyle]}>
+                <TouchableWithoutFeedback onPress={this.onChange}>
+                  <Image
+                      source={source}
+                      style={[styles.checkbox, this.props.checkboxStyle]}
+                  />
+                </TouchableWithoutFeedback>
+                  <View style={styles.labelContainer}>
+                    <Text
+                        numberOfLines={this.props.labelLines}
+                        style={[styles.label, this.props.labelStyle]}
+                    >
+                      {this.props.label}
+                    </Text>
+                  </View>
+              </View>
+          );
+    }
+
+    return (
+          <View
+              style={styles.flexContainer}
+              underlayColor={this.props.underlayColor}
+          >
+              {container}
+          </View>
+    );
+  }
 }
 
-var styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 5,
-    },
-    checkbox: {
-        width: 26,
-        height: 26
-    },
-    labelContainer: {
-        marginLeft: 10,
-        marginRight: 10,
-    },
-    label: {
-        fontSize: 15,
-        color: 'grey'
-    }
-});
-
 CheckBox.propTypes = {
-    label: PropTypes.string,
-    labelBefore: PropTypes.bool,
-    labelStyle: PropTypes.oneOfType([PropTypes.array,PropTypes.object,PropTypes.number]),
-    labelLines: PropTypes.number,
-    checkboxStyle: PropTypes.oneOfType([PropTypes.array,PropTypes.object,PropTypes.number]),
-    containerStyle: PropTypes.oneOfType([PropTypes.array,PropTypes.object,PropTypes.number]),
-    checked: PropTypes.bool,
-    checkedImage: PropTypes.number,
-    uncheckedImage: PropTypes.number,
-    underlayColor: PropTypes.string,
-    onChange: PropTypes.func
+  checkboxStyle: PropTypes.oneOfType([PropTypes.array, PropTypes.object, PropTypes.number]),
+  checked: PropTypes.bool,
+  checkedImage: PropTypes.number,
+  containerStyle: PropTypes.oneOfType([PropTypes.array, PropTypes.object, PropTypes.number]),
+  label: PropTypes.string,
+  labelBefore: PropTypes.bool,
+  labelLines: PropTypes.number,
+  labelStyle: PropTypes.oneOfType([PropTypes.array, PropTypes.object, PropTypes.number]),
+  onChange: PropTypes.func,
+  uncheckedImage: PropTypes.number,
+  underlayColor: PropTypes.string,
 };
 
 CheckBox.defaultProps = {
-    label: 'Label',
-    labelLines: 1,
-    labelBefore: false,
-    checked: null,
-    checkedImage: CB_ENABLED_IMAGE,
-    uncheckedImage: CB_DISABLED_IMAGE,
-    underlayColor: 'white'
+  checked: null,
+  checkedImage: CB_ENABLED_IMAGE,
+  label: 'Label',
+  labelBefore: false,
+  labelLines: 1,
+  uncheckedImage: CB_DISABLED_IMAGE,
+  underlayColor: 'white',
 };
 
 module.exports = CheckBox;
